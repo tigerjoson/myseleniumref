a3a12c8b7d1d14b46ead8a5b03fa3e333dc5953a
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2024-10-08 20:34:26 +0800

    Update keyboardclipboard.js

--- keyboardclipboard.js
+++ keyboardclipboard.js
@@ -3,7 +3,8 @@
 	await search_bar.sendKeys(Key.CONTROL, 'a');
 	//press BACK_SPACE
 	await search_bar.sendKeys(Key.BACK_SPACE);
-	//
+	//type "cloud computing"
 	await search_bar.sendKeys("cloud computing");
+	//press enter
 	await search_bar.sendKeys(Key.ENTER);
 	
